80fc1cc86747bccfb1d8d2fb3ca3ccc7f506990f
Albina Januzi <albina_januzi@hotmail.com>
2025-02-19 14:25:48 +0100

    Add mySkills Component

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landingPage.component';
 import { WhyMeComponent } from './why-me/why-me.component';
+import { MySkillsComponent } from './my-skills/my-skills.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [CommonModule, LandingPageComponent, WhyMeComponent],
+  imports: [CommonModule, LandingPageComponent, WhyMeComponent, MySkillsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
