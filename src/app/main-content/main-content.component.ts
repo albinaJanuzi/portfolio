551332048ec98aa6b7aed47b6dd0ba2fd34ae0a8
Albina Januzi <albina_januzi@hotmail.com>
2025-02-19 14:21:53 +0100

    Why me Section

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landingPage.component';
+import { WhyMeComponent } from './why-me/why-me.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [CommonModule, LandingPageComponent],
+  imports: [CommonModule, LandingPageComponent, WhyMeComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
