41708c3a9221d9d889d2e05d65675ce217a0b9e2
Albina Januzi <albina_januzi@hotmail.com>
2025-02-20 14:48:44 +0100

    Navbar Navigate

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -6,10 +6,12 @@ import { MySkillsComponent } from './my-skills/my-skills.component';
 import { MyProjectsComponent } from './my-projects/my-projects.component';
 import { FeedbackComponent } from './feedback/feedback.component';
 import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-main-content',
-  imports: [CommonModule, LandingPageComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, FeedbackComponent, ContactComponent],
+  imports: [CommonModule, LandingPageComponent, WhyMeComponent, MySkillsComponent,
+    MyProjectsComponent, FeedbackComponent, ContactComponent, FooterComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
