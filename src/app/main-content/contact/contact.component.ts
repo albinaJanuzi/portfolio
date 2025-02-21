0662a2d4eec1a775f72aefe97311a9ea9886a61a
Albina Januzi <albina_januzi@hotmail.com>
2025-02-21 13:59:16 +0100

    Input & Text Area red when not valid

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-contact',
   imports: [FormsModule, CommonModule],
@@ -13,13 +14,18 @@ export class ContactComponent {
   formData = { name: '', email: '', message: '', agreed: false };
   isSubmitted = false;
 
-  onSubmit() {
-    if (!this.formData.agreed) {
-      this.checkboxState = 'error'; 
-    } else {
-      console.log('Form Data:', this.formData);
-      alert('Message sent successfully!');
+  onSubmit(form: NgForm) {
+    this.isSubmitted = true;
+
+    if (!form.valid || !this.formData.agreed) {
+      if (!this.formData.agreed) {
+        this.checkboxState = 'error'; // Fehleranzeige für Checkbox
+      }
+      return; // Formular wird nicht abgeschickt
     }
+
+    console.log('Form Data:', this.formData);
+    alert('Message sent successfully!');
   }
 
   toggleCheckbox() {
